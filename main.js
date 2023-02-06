5b2b4873a0c4a4c228c056146b6f0411f490642b
PolegolasXD <polegolasplays@gmail.com>
2023-02-06 18:10:18 -0300

    feat: responsividade -tornando pagina responsiva para mobile -corrigindo bugs de dimensão

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ var btn = document.querySelector("#back-to-top");
 btn.addEventListener("click", function () {
     window.scrollTo(0, 0);
 })
+
+
+//menu mobile
+
+function myFunction() {
+    var x = document.getElementById('myLinks')
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+
+    }
+}
